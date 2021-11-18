14f34c49ca9523735a0a8dd89313c5ed8cafce70
JamieB <jamie.bowen@forgerock.com>
2021-11-18 14:34:38 +0000

    45: Regenerate funds conformation models from 3.1.8r5 swagger
    Issue: https://github.com/OpenBankingToolkit/openbanking-toolkit/issues/45

--- src/main/java/uk/org/openbanking/datamodel/fund/OBFundsConfirmationConsentResponse1.java
+++ src/main/java/uk/org/openbanking/datamodel/fund/OBFundsConfirmationConsentResponse1.java
@@ -32,120 +32,125 @@ import java.util.Objects;
 /**
  * OBFundsConfirmationConsentResponse1
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-05-20T10:02:34.555654+01:00[Europe/London]")
-
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2021-11-18T09:57:44.731259Z[Europe/London]")
 public class OBFundsConfirmationConsentResponse1 {
-  // validation within OBFundsConfirmationConsentDataResponse1 now mirrors OBFundsConfirmationConsentResponse1Data (to minimise impact)
-  @JsonProperty("Data")
-  private OBFundsConfirmationConsentDataResponse1 data; // hand-edited (avoided switch to OBFundsConfirmationConsentResponse1Data)
+    @JsonProperty("Data")
+    private OBFundsConfirmationConsentResponse1Data data;
 
-  @JsonProperty("Links")
-  private Links links;
+    @JsonProperty("Links")
+    private Links links; // Use links from accounts to save class duplication
 
-  @JsonProperty("Meta")
-  private Meta meta;
+    @JsonProperty("Meta")
+    private Meta meta; // Use class from accounts to save class duplicaition
 
-  public OBFundsConfirmationConsentResponse1 data(OBFundsConfirmationConsentDataResponse1 data) {
-    this.data = data;
-    return this;
-  }
-
-  /**
-   * Get data
-   *
-   * @return data
-   */
-  @ApiModelProperty(required = true, value = "")
-  @NotNull
-  @Valid
-  public OBFundsConfirmationConsentDataResponse1 getData() {
-    return data;
-  }
-
-  public void setData(OBFundsConfirmationConsentDataResponse1 data) {
-    this.data = data;
-  }
-
-  public OBFundsConfirmationConsentResponse1 links(Links links) {
-    this.links = links;
-    return this;
-  }
-
-  /**
-   * Get links
-   *
-   * @return links
-   */
-  @ApiModelProperty(value = "")
-  @Valid
-  public Links getLinks() {
-    return links;
-  }
-
-  public void setLinks(Links links) {
-    this.links = links;
-  }
-
-  public OBFundsConfirmationConsentResponse1 meta(Meta meta) {
-    this.meta = meta;
-    return this;
-  }
-
-  /**
-   * Get meta
-   *
-   * @return meta
-   */
-  @ApiModelProperty(value = "")
-  @Valid
-  public Meta getMeta() {
-    return meta;
-  }
-
-  public void setMeta(Meta meta) {
-    this.meta = meta;
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    public OBFundsConfirmationConsentResponse1 data(OBFundsConfirmationConsentResponse1Data data) {
+        this.data = data;
+        return this;
     }
-    if (o == null || getClass() != o.getClass()) {
-      return false;
+
+    /**
+     * Get data
+     *
+     * @return data
+     */
+    @ApiModelProperty(required = true, value = "")
+    @NotNull
+
+    @Valid
+
+    public OBFundsConfirmationConsentResponse1Data getData() {
+        return data;
     }
-    OBFundsConfirmationConsentResponse1 obFundsConfirmationConsentResponse1 = (OBFundsConfirmationConsentResponse1) o;
-    return Objects.equals(this.data, obFundsConfirmationConsentResponse1.data) &&
-        Objects.equals(this.links, obFundsConfirmationConsentResponse1.links) &&
-        Objects.equals(this.meta, obFundsConfirmationConsentResponse1.meta);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(data, links, meta);
-  }
-
-  @Override
-  public String toString() {
-    StringBuilder sb = new StringBuilder();
-    sb.append("class OBFundsConfirmationConsentResponse1 {\n");
-    
-    sb.append("    data: ").append(toIndentedString(data)).append("\n");
-    sb.append("    links: ").append(toIndentedString(links)).append("\n");
-    sb.append("    meta: ").append(toIndentedString(meta)).append("\n");
-    sb.append("}");
-    return sb.toString();
-  }
-
-  /**
-   * Convert the given object to string with each line indented by 4 spaces
-   * (except the first line).
-   */
-  private String toIndentedString(Object o) {
-    if (o == null) {
-      return "null";
+    public void setData(OBFundsConfirmationConsentResponse1Data data) {
+        this.data = data;
     }
-    return o.toString().replace("\n", "\n    ");
-  }
+
+    public OBFundsConfirmationConsentResponse1 links(Links links) {
+        this.links = links;
+        return this;
+    }
+
+    /**
+     * Get links
+     *
+     * @return links
+     */
+    @ApiModelProperty(value = "")
+
+    @Valid
+
+    public Links getLinks() {
+        return links;
+    }
+
+    public void setLinks(Links links) {
+        this.links = links;
+    }
+
+    public OBFundsConfirmationConsentResponse1 meta(Meta meta) {
+        this.meta = meta;
+        return this;
+    }
+
+    /**
+     * Get meta
+     *
+     * @return meta
+     */
+    @ApiModelProperty(value = "")
+
+    @Valid
+
+    public Meta getMeta() {
+        return meta;
+    }
+
+    public void setMeta(Meta meta) {
+        this.meta = meta;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        OBFundsConfirmationConsentResponse1 obFundsConfirmationConsentResponse1 = (OBFundsConfirmationConsentResponse1) o;
+        return Objects.equals(this.data, obFundsConfirmationConsentResponse1.data) &&
+                Objects.equals(this.links, obFundsConfirmationConsentResponse1.links) &&
+                Objects.equals(this.meta, obFundsConfirmationConsentResponse1.meta);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(data, links, meta);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("class OBFundsConfirmationConsentResponse1 {\n");
+
+        sb.append("    data: ").append(toIndentedString(data)).append("\n");
+        sb.append("    links: ").append(toIndentedString(links)).append("\n");
+        sb.append("    meta: ").append(toIndentedString(meta)).append("\n");
+        sb.append("}");
+        return sb.toString();
+    }
+
+    /**
+     * Convert the given object to string with each line indented by 4 spaces
+     * (except the first line).
+     */
+    private String toIndentedString(Object o) {
+        if (o == null) {
+            return "null";
+        }
+        return o.toString().replace("\n", "\n    ");
+    }
 }
 
